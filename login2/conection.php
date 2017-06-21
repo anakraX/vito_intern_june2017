ade64fa9cb6b737e16a89d2de517cc932d7449b3
Vito Raditya Fauzan <vitoraditya.f@gmail.com>
2017-06-21 20:24:34 +0700

    tugas_8-9

---
+++ login2/conection.php
@@ -0,0 +1,1 @@
+<?php $host = "localhost"; $user = "root"; $pass = ""; $db = "tugas8"; $konek = mysql_connect($host, $user, $pass) or die ('Koneksi Gagal! '); mysql_select_db($db); ?>
---
+++ login2/main.php
@@ -0,0 +1,8 @@
+<?php session_start();
+if(!isset($_SESSION['username'])) {
+header('location:login.php'); }
+else { $username = $_SESSION['username']; }
+require_once("koneksi.php");
+$query = mysql_query("SELECT * FROM user WHERE username = '$username'");
+$hasil = mysql_fetch_array($query);
+?>
